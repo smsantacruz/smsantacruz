6d99198ab57c752717c560da219e0e0442a20ca2
Sergio Santa Cruz <smsantacruz@INVAP.COM.AR>
2022-04-27 14:12:35 -0300

    first commit

---
+++ build/precache-manifest.061c7fc6a7efcf0ce2892fbb0281846e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c755f49c2fe78705a0139d2f23e5b04d",
+    "url": "/to_do_list/index.html"
+  },
+  {
+    "revision": "a8637d4de77e4b277d7d",
+    "url": "/to_do_list/static/js/2.f877089a.chunk.js"
+  },
+  {
+    "revision": "914a85462fc888825cb3a498c2b9869c",
+    "url": "/to_do_list/static/js/2.f877089a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbc9a1476621bd04dac9",
+    "url": "/to_do_list/static/js/main.fc229b33.chunk.js"
+  },
+  {
+    "revision": "26f08fdd5d1abead919c",
+    "url": "/to_do_list/static/js/runtime-main.ef9985dc.js"
+  }
+]);
---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react'
+import { createGlobalStyle } from 'styled-components'
+
+import { generate as id } from 'shortid'
+import allColors from './styles/colors'
+import FormTask from './components/FormTask'
+import Task from './components/Task'
+
+const GlobalStyle = createGlobalStyle`
+    body{
+        font-family:sans-serif;
+        background-color: #222;
+        color:${allColors.mainColor};
+        text-align: center;
+        margin:0;
+    }
+`
+
+const App = () => {
+
+    const [colorSelected, setColorSelected] = useState(allColors.colors[0])
+    const [tasks, setTasks] = useState([])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (e.target.title.value.trim() !== '') {
+            createNewTask(e.target.title.value)
+            e.target.title.value = ''
+        }
+    }
+
+    const createNewTask = (title) => {
+        const newTask = {
+            id: id(),
+            title,
+            color: colorSelected,
+            done: false
+        }
+
+        const allTasks = [...tasks, newTask]
+
+        setTasks(allTasks)
+    }
+
+    const handleCompleteTask = (id) => {
+        const currentTasks = [...tasks]
+        const task = currentTasks.find(task => task.id === id)
+        const index = currentTasks.indexOf(task)
+
+        currentTasks[index].done = !currentTasks[index].done
+
+        setTasks(currentTasks)
+    }
+
+    const handleDeleteTask = (id) => {
+        let currentTasks = [...tasks]
+        currentTasks = currentTasks.filter(task => task.id !== id)
+
+        setTasks(currentTasks)
+    }
+
+    const handleChangeColor = (color) => {
+        setColorSelected(color)
+    }
+
+    return (
+        <>
+            <GlobalStyle />
+            <h1>To-do-list Challenge!</h1>
+            <h3>From: Sergio Santa Cruz</h3>
+            <FormTask
+                handleChangeColor={handleChangeColor}
+                handleSubmit={handleSubmit}
+                colorSelected={colorSelected}
+            />
+
+            {tasks.length === 0 && <p>You dont have any task!</p>}
+            {
+                tasks.map(task => (
+                    <Task
+                        key={id()}
+                        done={task.done}
+                        title={task.title}
+                        color={task.color}
+                        handleCompleteTask={() => handleCompleteTask(task.id)}
+                        handleDeleteTask={() => handleDeleteTask(task.id)}
+                    />
+                ))
+            }
+        </>
+
+    )
+}
+
+export default App;
